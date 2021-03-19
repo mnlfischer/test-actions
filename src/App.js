79082011672d209c83331d70524890cee15cd7fc
Manuel Fischer <fischer@neoskop.de>
2021-03-19 16:44:53 +0100

    changed text again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >dasd
-          Learn React 
+          Learn React adas
         </a>
       </header>
     </div>
