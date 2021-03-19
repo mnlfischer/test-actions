87ccba00c69e60dc785cac86248d00bb5ad3bee2
Manuel Fischer <fischer@neoskop.de>
2021-03-19 14:16:44 +0100

    removed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >dasd
-          Learn React testext
+          Learn React 
         </a>
       </header>
     </div>
